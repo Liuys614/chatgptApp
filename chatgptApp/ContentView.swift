97bbaf8a5a22382c0559fefc410c021101f49543
Luffy <liuys614@gmail.com>
2023-03-07 20:01:32 -0500

    Initial Commit

---
+++ chatgptApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  chatgptApp
+//
+//  Created by Louis Liu on 2023/3/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ chatgptApp/chatgptAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatgptAppApp.swift
+//  chatgptApp
+//
+//  Created by Louis Liu on 2023/3/7.
+//
+
+import SwiftUI
+
+@main
+struct chatgptAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
